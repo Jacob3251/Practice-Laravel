8f5345c3f8e14bf8a1a7f14b7f90dc8fbf839225
Jacob3251 <jacobfrye3251@gmail.com>
2023-09-09 03:01:05 +0600

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Crud</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1 class="text-center text-[48px]">Products Table</h1>
+    <h3 class="text-center text-[24px]">Product Information</h3>
+</body>
+
+</html>
